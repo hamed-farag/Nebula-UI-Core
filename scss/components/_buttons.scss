cf3620a530eef93d519347cd1e4b0a928fcd3a3a
Hamed Farag <hamed.farag.2009@gmail.com>
2018-05-31 20:41:45 +0200

    Enhance Style Guide

---
+++ scss/components/_buttons.scss
@@ -0,0 +1,23 @@
+// Button
+//
+// Your standard button suitable for clicking.
+//
+// :hover   - Highlights when hovering.
+//
+// Markup: button.html
+//
+// Style guide: Components.Button
+
+.button {
+    background-color: #4CAF50;
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    &:hover {
+        background-color: #424EDA;
+    }
+}
