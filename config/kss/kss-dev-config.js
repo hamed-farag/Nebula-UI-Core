5d6dfce88435c5341b7589ce557a214148370a3e
Hamed Farag <hamed.farag.2009@gmail.com>
2018-07-17 17:03:34 +0200

    Enhance Style Guide Styles Upgrade Version number

--- config/kss/kss-dev-config.js
+++ config/kss/kss-dev-config.js
@@ -4,5 +4,6 @@ kssCommon.css = [
     "https://fonts.googleapis.com/css?family=Lato",
     "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
     "../dist/nebula-ui.css", 
+    "../config/kss/customStyles/kss-hub.css"
 ];
 module.exports = kssCommon;
--- config/kss/kss-prod-config.js
+++ config/kss/kss-prod-config.js
@@ -4,6 +4,7 @@ kssCommon.destination = "../../dist/prod/nebula-ui/" + pkg.version + "/doc";
 kssCommon.css = [
     "https://fonts.googleapis.com/css?family=Lato",
     "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
-    "../nebula-ui.min.css"
+    "../nebula-ui.min.css",
+    "../kss/kss-hub.css"
 ];
 module.exports = kssCommon;
