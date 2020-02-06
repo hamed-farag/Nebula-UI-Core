b3396d792300cfa15fcd1d01b503994946690f59
marina alber <marina.alber@outlook.com>
2020-02-06 18:58:05 +0200

    - added "natives" package. - Updated the global variabes. - Updated font-awesome cdn. - Updated UI.

--- config/kss/kss-dev-config.js
+++ config/kss/kss-dev-config.js
@@ -2,7 +2,7 @@ var kssCommon = require("./kss-common-config");
 kssCommon.destination = "../../docs";
 kssCommon.css = [
     "https://fonts.googleapis.com/css?family=Lato",
-    "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
+    "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
     "../dist/nebula-ui.css", 
     "../config/kss/customStyles/kss-hub.css"
 ];
